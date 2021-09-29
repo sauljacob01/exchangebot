1ea6bb40b6a5d660e1275cd27b439d459d2b10f1
RickDelnoy <90474106+RickDelnoy@users.noreply.github.com>
2021-09-29 14:08:51 +0200

    Update exchangebot.py

--- exchangebot.py
+++ exchangebot.py
@@ -19,12 +19,6 @@ from PIL import Image, ImageDraw, ImageFont
 from io import BytesIO
 from discord_components import DiscordComponents, Button, ButtonStyle
 
-#DATABASE
-#DBuser = "postgres"
-#DBpassword = "getaccessbase946doge+ec"
-#DBdatabase = "exchangebottoken"
-#Host = "localhost"
-
 #INFO
 SERVERID = 876134892939923477
 WELCOMECHANNELID = 882286388177956956
@@ -46,12 +40,6 @@ intents.members = True
 bot = commands.Bot(command_prefix="ec.", intents=discord.Intents.all(), case_insensitive=True, owner_id=875422606159912990)
 slash = SlashCommand(bot, sync_commands=True)
 bot.remove_command("help")
-
-#Bot Token Reader
-def read_token():
-    with open("exchangebottoken.txt", "r") as f:
-        lines = f.readlines()
-        return lines[0].strip()
 
 #Bot Start and Status
 @bot.event
@@ -193,126 +181,19 @@ async def kick_error(ctx, error):
 
 @bot.command()
 async def wanted(ctx, user: discord.Member=None):
-    if user == None:
-        user = ctx.author
-
-    wanted = Image.open("wanted.jpg")
-    asset = user.avatar_url_as(size=128)
-    data = BytesIO(await asset.read())
-    pfp = Image.open(data)
-    pfp = pfp.resize((177,177))
-
-    wanted.paste(pfp, (120,212))
-    wanted.save("profile.jpg")
-    await ctx.send(file = discord.File("profile.jpg"))
-
-# @bot.command()
-# async def accinfo(ctx):
-#     with open("ledger.json", "r") as f:
-#         data = json.load(f)
-#         await ctx.send(f"""
-# UserID: {data[UserID]},
-# Expoints: {data[Expoints]},
-# Trust Score: {data[]},
-# Transactions: {data[transactions]}""")
+    await ctx.send("Command Not Avaliable Now")
 
 @bot.command()
 async def expointsinfo(ctx):
-    embed = discord.Embed(title="Expoints Info", description="""
-    List of minimum expoints to withdraw
-    Bitcoin: 5000 expoints
-    Dogecoin: 2000 expoints
-    Discord Nitro Classic: 1000 expoints
-    Discord Nitro: 3000 expoints""", color=0x0075db)
-    await ctx.send(embed=embed)
-
+    await ctx.send("Command Not Avaliable Now")
 
 @bot.command()
 async def createaccount(ctx):
-    with open("ledger.json", "r") as file:
-        data = json.load(file)
-        userid = str(ctx.author.id)
-
-    length = len(data)
-    membernum = f"Account {length}"
-    if userid in data:
-        await ctx.send("You already have created an account")
-    else:
-        data[userid] = []
-        data[userid].append({
-            "Name": str(ctx.author),
-            "UserID": str(ctx.author.id),
-            "AccountNum": str(membernum),
-            "Expoints": str(expoints),
-            "TrustScore": str(trustscore),
-            "Transactions": str(transactions),
-            "Balance": str(balance),
-            "Date": str(datetime.datetime.utcnow().strftime("%d/%m/%Y")),
-            "Time": str(datetime.datetime.utcnow().strftime("%H:%M:%S")),
-            "Timestamp": str(datetime.datetime.utcnow())
-            # "Date": str(datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f'))
-        })
-
-        with open("ledger.json", "w") as file:
-            json.dump(data, file, indent=4, default=str)
-            file.close()
-            await ctx.send("Your account has been created! Check your DMs")
-
-        with open("ledger.json", "r") as file:
-            data = json.load(file)
-            member = ctx.message.author
-            for ledger in data[userid]:
-                if userid in data:
-                    embed = discord.Embed(title="""--------------
-Your Account Info""", description=f"""
-                                        Username: {ledger["Name"]}
-                        UserID: {ledger["UserID"]}
-                        Expoints: {ledger["Expoints"]}
-                        Trust Score: {ledger["TrustScore"]}/100
-                        Transactions: {ledger["Transactions"]}
-                    --------------""", color=0x0075db)
-
-                    embed.set_footer(text=f"""{ledger['AccountNum']}
-Account was created on {ledger['Date']} at {ledger['Time']}""")
-
-                    embed.set_author(name=ctx.author.display_name,
-                                     url="https://discord.com/channels/876134892939923477/880492417030180955",
-                                     icon_url=ctx.author.avatar_url)
-                    await member.create_dm()
-                    await member.dm_channel.send(embed=embed)
+    await ctx.send("Command Not Avaliable Now")
 
 @bot.command()
 async def accountinfo(ctx, user: discord.Member=None):
-    userid = str(ctx.author.id)
-    if user == None:
-        userid = str(ctx.author.id)
-
-    with open("ledger.json", "r") as file:
-        data = json.load(file)
-        member = ctx.message.author
-        # length = len(data)
-        # membernum = f"Account {length}"
-
-        for ledger in data[userid]:
-            embed = discord.Embed(title="""--------------
-Your Account Info""", description=f"""
-                    Username: {ledger["Name"]}
-    UserID: {ledger["UserID"]}
-    Expoints: {ledger["Expoints"]}
-    Trust Score: {ledger["TrustScore"]}/100
-    Transactions: {ledger["Transactions"]}
---------------""", color=0x0075db)
-
-            embed.set_footer(text=f"""{ledger['AccountNum']}
-Account was created on {ledger['Date']} at {ledger['Time']}""")
-
-            embed.set_author(name=ctx.author.display_name, url="https://discord.com/channels/876134892939923477/880492417030180955",
-                             icon_url=ctx.author.avatar_url)
-            await member.create_dm()
-            await member.dm_channel.send(embed=embed)
-
-
-
+    await ctx.send("Command Not Avaliable Now")
 
 
 @slash.slash(name="ping", description="Displays the ping speed in milliseconds(ms)")
@@ -321,5 +202,4 @@ async def _ping(ctx):
 
 
 # Bot Token
-token = read_token()
-bot.run(token)
+bot.run(os.environ['ExchangeToken'])
